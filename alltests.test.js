89db3da94e4192c6a48aea88143a6e72e8d41ee3
simoneburrai <burrai.s98@gmail.com>
2025-09-19 14:59:39 +0200

    feat: test4, 5, 6 and 7 completed, test 7 added check for keys in post object and throwing error if id is not a number

--- alltests.test.js
+++ alltests.test.js
@@ -1,4 +1,11 @@
-const {getInitials, createSlug, average} = require("./functionsToTest.js")
+const {
+    posts,
+    getInitials, 
+    createSlug,
+    average, 
+    createSlugHyphen,
+    isPalindrome,
+findPostById} = require("./functionsToTest.js")
 
 
 //GET INITIALS
@@ -21,3 +28,46 @@ test("CreateSlug restituisce una stringa in lowercase senza spazi", ()=>{
 test("Average calcola la media aritmetica dato un array di numeri", ()=>{
     expect(average([1, 2, 3, 6])).toBe(3);
 })
+
+
+//CREATE SLUG WITH - INSTEAD OF SPACES
+test("La funzione createSlugHyphen sostituisce gli spazi con il trattino '-' ", ()=>{
+    expect(createSlugHyphen(" Ciao sono simone")).toBe("ciao-sono-simone");
+})
+
+
+//ISPALINDROME
+test("La funzione isPalindrome verifica se una determinata stringa sia palindroma o no", ()=>{
+    expect(isPalindrome("Anna")).toBeTruthy();
+    expect(isPalindrome("simone")).toBeFalsy();
+})
+
+test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", ()=>{
+    expect(()=>createSlugHyphen("Questo è il titolo da controllare!!!")).toThrow()
+})
+
+
+
+
+
+test(" La funzione findPostById restituisce il post corretto dato l’array di post e l’id.", ()=>{
+    expect(findPostById(posts, 3)).toEqual({
+    id: 3,
+    title: "post strano",
+    slug: "post-strano"
+})
+});
+
+test("Ogni post ha le proprietà id, title e slug", () => {
+    posts.forEach(post => {
+        expect(post).toHaveProperty("id");
+        expect(post).toHaveProperty("title");
+        expect(post).toHaveProperty("slug");
+    });
+});
+
+
+test("La funzione lancia un errore se l'id non è un numero", () => {
+    expect(() => findPostById(posts, "4")).toThrow();
+    expect(() => findPostById(posts, null)).toThrow();
+});
--- functionsToTest.js
+++ functionsToTest.js
@@ -27,9 +27,50 @@ const average = (arr)=>{
     return (arr.reduce((acc, num) => acc + num, 0))/arr.length;
 }
 
+const createSlugHyphen= str =>{
+    const invalidSymbols = "!@#$%^&*()_+={}[]|:;'<>,.?/`~".split("");
+    const notTitleValid = str.split("").some(char=> invalidSymbols.includes(char));
+    if(!str || notTitleValid){
+        throw new Error("Inserire un titolo non vuoto e valido");
+    }
+   return str.toLowerCase().trim().split(" ").join("-");
+}
+
+const isPalindrome = str => {
+    return str.toLowerCase() === str.split("").reverse().join("").toLowerCase()
+}
+
+const findPostById= (posts, id)=>{
+    if (typeof id !== "number") {
+            throw new Error("L'id deve essere un numero");
+        }
+    return posts.find(post => post.id === id)
+}
+
+const posts =[
+    {
+    id: 1,
+    title: "post brutto",
+    slug: "post-brutto"
+}, 
+{
+    id: 2,
+    title: "post bello",
+    slug: "post-bello"
+},
+ {
+    id: 3,
+    title: "post strano",
+    slug: "post-strano"
+}];
+
 
 module.exports = {
+    posts,
     getInitials,
     createSlug,
-    average
+    average,
+    createSlugHyphen,
+    isPalindrome,
+    findPostById
 }
